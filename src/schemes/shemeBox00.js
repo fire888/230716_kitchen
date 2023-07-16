34467bfb5a73f50b4190667c55ef64036a7a0e7f
vasilii <porokh.vs@gmail.com>
2023-07-16 21:38:03 +0300

    procees

---
+++ src/schemes/shemeBox00.js
@@ -0,0 +1,17 @@
+export const BOX00_SCHEME = {
+    w: 300,
+    d: 700,
+    h: 50,
+    facetS: {
+        type: 'facet11',
+    },
+    facetW: {
+        type: 'facet11',
+    },
+    facetN: {
+        type: 'facet11',
+    },
+    facetE: {
+        type: 'facet11',
+    },
+}
